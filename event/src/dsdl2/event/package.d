11c0e5fe8ca62eb16116df07f41d0e52e082ef48
Unknown <JonathanILevi@gmail.com>
2018-02-08 19:26:25 -0600

    Added structure for event subpackage.

---
+++ event/src/dsdl2/event/package.d
@@ -0,0 +1,21 @@
+/**
+The Event submodule of DSDL2.  To handle events (rather necessary!).
+
+
+
+*/
+
+
+module dsdl2.event;
+
+
+import std.stdio;
+
+
+import cst_;
+
+
+import dsdl2.window	;
+import derelict.sdl2.sdl	;
+import std.string	: toStringz;
+
